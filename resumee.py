3a1e0288001672c4a453aa1b9d68ec0a93841d69
Fsadness <f.sadness@qq.com>
2023-07-04 15:20:28 +0800

    Add UIE to resumee, see example format at test.py

---
+++ resumee.py
@@ -0,0 +1,57 @@
+import os
+from typing import List
+import torch
+from transformers import AutoTokenizer
+
+from text_extraction.doc_to_text import doc_to_text
+from inference import inference, event_extract_example, information_extract_example, ner_example
+
+
+device = 'cuda:0'
+saved_model_path = 'UIE/checkpoints/DuIE/model_best/'
+tokenizer = AutoTokenizer.from_pretrained(saved_model_path) 
+model = torch.load(os.path.join(saved_model_path, 'model.pt'))
+model.to(device).eval()
+
+
+class Resumee(object):
+    # example token: {'ner': ['token'], 'information': {'token': ['information']}, 'event': {'token': ['event']}}
+    def __init__(self, path, token={'ner': [], 'information': {}, 'event': {}}): 
+        self.path = path
+        self.name = os.path.basename(self.path)
+        _, self.ext = os.path.splitext(self.name)
+        self.text = doc_to_text(self.path, self.ext)
+        self.token = token
+        self.ner, self.information, self.event = self.extraction(token)
+    
+    def extraction(self, token, save=False):
+        try:
+            schema = token['ner']
+            try:
+                ner = ner_example(model, tokenizer, device, sentence=self.text, schema=schema)
+            except IndexError:
+                ner = {}
+        except KeyError:
+            ner = {}
+        
+        try:
+            schema = token['information']
+            information = information_extract_example(model, tokenizer, device, sentence=self.text, schema=schema)
+        except KeyError:
+            information = {}
+        
+        try:
+            schema = token['event']
+            event = event_extract_example(model, tokenizer, device, sentence=self.text, schema=schema)
+        except KeyError:
+            event = {}
+        
+        if save:
+            self.ner = ner
+            self.information = information
+            self.event = event
+        
+        return ner, information, event
+            
+        
+        
---
+++ test.py
@@ -0,0 +1,27 @@
+import os
+from typing import List
+import torch
+from transformers import AutoTokenizer
+
+from resumee import Resumee
+
+
+test = Resumee(
+                path = 'data/dataset_CV/CV/16.docx',
+                # path = 'text_extraction/temp.pdf',
+                token = {
+                    'ner': ['人物', '教育背景触发词', '工作经历触发词', '日期'], 
+                    'information': {
+                        '人物': ['性别', '出生年月', '年龄', '就业意向','民族', ' 电话', '政治面貌', '邮箱', '住址'],
+                    }, 
+                    'event': {
+                        '教育背景触发词': ['学校', '阶段', '专业', '主修课程', '时间'],
+                        '工作经历触发词': ['公司', '职位', '工作内容', '时间']
+                    }
+                }
+            )
+
+print(test.text)
+print('[+] NER Results: ', test.ner)
+print('[+] Information-Extraction Results: ', test.information)
+print('[+] Event-Extraction Results: ', test.event)
