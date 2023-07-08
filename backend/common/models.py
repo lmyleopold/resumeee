dae105de25271e83f2f4a490e01902ad8c9cf9ab
lmyleopold <92246296+lmyleopold@users.noreply.github.com>
2023-07-09 00:47:32 +0800

    update Job model

--- backend/common/models.py
+++ backend/common/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Applicant(models.Model):
     # 应聘者姓名
@@ -10,8 +11,13 @@ class Applicant(models.Model):
     job = models.CharField(max_length=200)
     # more to add
 
+
 class Job(models.Model):
     # 工作名
     name = models.CharField(max_length=200)
     # 工作描述
-    description = models.CharField(max_length=400)
+    # description = models.CharField(max_length=400)
+    # 岗位职责
+    requirements = models.TextField(max_length=400)
+    # 任职要求
+    responsibilities = models.TextField(max_length=400)
