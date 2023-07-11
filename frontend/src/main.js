e299d30926b477489f1727b1eda41bf65f8fbd4a
Xinyang Li <lixinyang411@gmail.com>
2023-07-11 01:54:15 +0000

    feat: add component JobCard

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import { ElCollapseTransition } from 'element-plus'
 import App from './App.vue'
 
 const app = createApp(App).mount('#app')
+app.component(ElCollapseTransition.name, ElCollapseTransition)
